8273cf43c8044db47b4e1a616041d8348c95e19d
Daniel Bravo <daniel.bravo@pragma.com.co>
2025-08-21 15:53:01 -0500

    feat: archetype

---
+++ src/main/java/co/nequi/franquicias_api_hexagonal/FranquiciasApiHexagonalApplication.java
@@ -0,0 +1,13 @@
+package co.nequi.franquicias_api_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquiciasApiHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquiciasApiHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/nequi/franquicias_api_hexagonal/FranquiciasApiHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.nequi.franquicias_api_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasApiHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
